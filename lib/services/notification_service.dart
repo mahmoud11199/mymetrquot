351546f127d285ac78a9b02ba656bb7f2c70b6e4
mahmoud11199 <91949713+mahmoud11199@users.noreply.github.com>
2026-03-04 16:44:35 +0200

    Comment out Firebase notification service usage

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,12 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationService {
   Future<void> initialize() async {
     try {
-      await Firebase.initializeApp();
-      await FirebaseMessaging.instance.requestPermission();
-      await FirebaseMessaging.instance.getToken();
+      // await Firebase.initializeApp();
+      // await FirebaseMessaging.instance.requestPermission();
+      // await FirebaseMessaging.instance.getToken();
     } catch (_) {
       // Firebase may be unconfigured in local/dev environments.
     }
